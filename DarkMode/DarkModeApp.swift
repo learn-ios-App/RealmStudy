d7b75b322a897de9702bc3d236f967299b718d9a
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-01-17 15:57:14 +0900

    Initial Commit

---
+++ DarkMode/DarkModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModeApp.swift
+//  DarkMode
+//
+//  Created by 渡邊魁優 on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct DarkModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
